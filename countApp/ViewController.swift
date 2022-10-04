d05e5efafb1bb2018b1cd11d1cb71ca3499a5818
Olga Kharitonova <o.kharitonova22@gmail.com>
2022-10-04 23:28:53 +0300

    Initial Commit

---
+++ countApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  countApp
+//
+//  Created by Olga Kharitonova on 04.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
