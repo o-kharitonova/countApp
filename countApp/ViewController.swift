14f2a88e3a7167352ddd7b1dd41ea163fadb0985
Olga Kharitonova <o.kharitonova22@gmail.com>
2022-10-04 23:52:07 +0300

    Написан код

--- countApp/ViewController.swift
+++ countApp/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    var count: Int = 0 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateLabel()
     }
-
-
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        count += 1
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        countLabel.text = "Значение счетчика: \(count)"
+    }
 }
 
